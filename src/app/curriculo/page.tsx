d3ad5fdac41f22ce635adc1cc586afe651cafc8b
diego <diego.fernandesjs@gmail.com>
2024-02-28 11:00:09 -0300

    teste

--- src/app/curriculo/page.tsx
+++ src/app/curriculo/page.tsx
@@ -1,105 +1,105 @@
-// 'use client'
-// import { LinkBtn } from "@/components/Main/Card/card-button"
-// import Training from "@/components/Main/Curriculo/training"
-// import Experience from "@/components/Main/Curriculo/experience"
-// import Skills from "@/components/Main/Curriculo/skills"
-// import styled from "styled-components"
+'use client'
+import { LinkBtn } from "@/components/Main/Card/card-button"
+import Training from "@/components/Main/Curriculo/training"
+import Experience from "@/components/Main/Curriculo/experience"
+import Skills from "@/components/Main/Curriculo/skills"
+import styled from "styled-components"
 
-// interface CurriculoProps {
+interface CurriculoProps {
 
-// }
+}
 
-// const Div = styled.div`
-//     background-color: #e1e6e3;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     padding-bottom: 1rem;
-//     @media screen and (min-width: 768px){
-//         padding-top: 110px;
-//     }
-// `
+const Div = styled.div`
+    background-color: #e1e6e3;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-bottom: 1rem;
+    @media screen and (min-width: 768px){
+        padding-top: 110px;
+    }
+`
 
-// const Container = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-// `
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
-// const TagDiv = styled.div`
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     gap: 0.5rem;
-//     padding: 2rem;
-// `
+const TagDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    padding: 2rem;
+`
 
-// const Square = styled.div`
-//     width: 20px;
-//     height: 20px;
-//     background-color: #274862;
-// `
-
-
-// const ContentLink = styled.div`
-//     display: flex;
-//     justify-content: space-between;
-//     align-items: center;
-//     padding-bottom: 2rem;
-//     width: 90%;
-//     @media screen and (min-width: 1024px){
-//         width: 50%;
-//     }
-// `
-
-// const LinkActive = styled(LinkBtn)`
-//     border: none;
-//     text-align: center;
-//     background-color: #274862;
-//     color: #fff;
-//     @media screen and (min-width: 768px){
-//         display: block;
-//         padding-top: 0.2em;
-//     }
-//     `
-
-// const Middle =  styled.div`
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     display: flex;
-//     padding: 2rem 0 2rem ;
-//     `
-
-// const TitleMiddle = styled.div`
-//     width: 90%;
-//     @media screen and (min-width: 1024px){
-//         width: 50%;
-//     }
-// `
+const Square = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: #274862;
+`
 
 
-// export default function Curriculo(props : CurriculoProps){
-//     return(
-//         <Div>
-//             <Container>
-//                 <TagDiv>
-//                     <Square></Square>
-//                     <h1>Currículo</h1>
-//                 </TagDiv>
-//                 <ContentLink>
-//                     <h3>Experiência</h3>
-//                     <LinkActive href="#">Baixar cv</LinkActive>
-//                 </ContentLink>
-//             </Container>
-//             <Experience/>
-//             <Middle>
-//                 <TitleMiddle>
-//                     <h3>Formação</h3>
-//                 </TitleMiddle>
-//             </Middle>
-//             <Training/>
-//             <Skills/>
-//         </Div>
-//     )
-// }
+const ContentLink = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 2rem;
+    width: 90%;
+    @media screen and (min-width: 1024px){
+        width: 50%;
+    }
+`
+
+const LinkActive = styled(LinkBtn)`
+    border: none;
+    text-align: center;
+    background-color: #274862;
+    color: #fff;
+    @media screen and (min-width: 768px){
+        display: block;
+        padding-top: 0.2em;
+    }
+    `
+
+const Middle =  styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    padding: 2rem 0 2rem ;
+    `
+
+const TitleMiddle = styled.div`
+    width: 90%;
+    @media screen and (min-width: 1024px){
+        width: 50%;
+    }
+`
+
+
+export default function Curriculo(props : CurriculoProps){
+    return(
+        <Div>
+            <Container>
+                <TagDiv>
+                    <Square></Square>
+                    <h1>Currículo</h1>
+                </TagDiv>
+                <ContentLink>
+                    <h3>Experiência</h3>
+                    <LinkActive href="#">Baixar cv</LinkActive>
+                </ContentLink>
+            </Container>
+            <Experience/>
+            <Middle>
+                <TitleMiddle>
+                    <h3>Formação</h3>
+                </TitleMiddle>
+            </Middle>
+            <Training/>
+            <Skills/>
+        </Div>
+    )
+}
--- src/components/Main/curriculo/Training.tsx
+++ src/components/Main/curriculo/Training.tsx
@@ -1,71 +1,71 @@
-// import styled from "styled-components"
+import styled from "styled-components"
 
-// const Container = styled.div`
-//     display: flex;
-//     align-items: center;
-//     flex-direction: column;
-//     gap: 1rem;
-// `
-// const ContainerContent = styled.div`
-//     width: 90%;
-//     background-color: #fff;
-//     @media screen and (min-width: 1024px){
-//         width: 50%;
-//     }
-// `
-// const Content =  styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     gap: 1rem;
-//     padding: 1rem;
-//     box-shadow: -6px 4px 10px rgba(0, 0, 0, 0.3);
-// `
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+`
+const ContainerContent = styled.div`
+    width: 90%;
+    background-color: #fff;
+    @media screen and (min-width: 1024px){
+        width: 50%;
+    }
+`
+const Content =  styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    box-shadow: -6px 4px 10px rgba(0, 0, 0, 0.3);
+`
 
-// const Emphasis = styled.p`
-//     color: #274862;
-//     font-weight: 600;
-// `
+const Emphasis = styled.p`
+    color: #274862;
+    font-weight: 600;
+`
 
-// export default function Training(){
-//     return(
-//         <Container>
-//             <ContainerContent>
-//                 <Content>
-//                     <Emphasis>2035 - Atual</Emphasis>
-//                     <p>CARGO</p>
-//                     <div>
-//                         <p>Nome da empresa</p>
-//                         <p>Localização da empresa</p>
-//                     </div>
-//                     <p>Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em Editar texto ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
-//                     <p>Sou um ótimo lugar para você contar a sua história e compartilhar sobre você.</p>
-//                 </Content>
-//             </ContainerContent>
-//             <ContainerContent>
-//                 <Content>
-//                     <Emphasis>2035 - Atual</Emphasis>
-//                     <p>CARGO</p>
-//                     <div>
-//                         <p>Nome da empresa</p>
-//                         <p>Localização da empresa</p>
-//                     </div>
-//                     <p>Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em Editar texto ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
-//                     <p>Sou um ótimo lugar para você contar a sua história e compartilhar sobre você.</p>
-//                 </Content>
-//             </ContainerContent>
-//             <ContainerContent>
-//                 <Content>
-//                     <Emphasis>2035 - Atual</Emphasis>
-//                     <p>CARGO</p>
-//                     <div>
-//                         <p>Nome da empresa</p>
-//                         <p>Localização da empresa</p>
-//                     </div>
-//                     <p>Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em Editar texto ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
-//                     <p>Sou um ótimo lugar para você contar a sua história e compartilhar sobre você.</p>
-//                 </Content>
-//             </ContainerContent>
+export default function Training(){
+    return(
+        <Container>
+            <ContainerContent>
+                <Content>
+                    <Emphasis>2035 - Atual</Emphasis>
+                    <p>CARGO</p>
+                    <div>
+                        <p>Nome da empresa</p>
+                        <p>Localização da empresa</p>
+                    </div>
+                    <p>Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em Editar texto ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
+                    <p>Sou um ótimo lugar para você contar a sua história e compartilhar sobre você.</p>
+                </Content>
+            </ContainerContent>
+            <ContainerContent>
+                <Content>
+                    <Emphasis>2035 - Atual</Emphasis>
+                    <p>CARGO</p>
+                    <div>
+                        <p>Nome da empresa</p>
+                        <p>Localização da empresa</p>
+                    </div>
+                    <p>Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em Editar texto ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
+                    <p>Sou um ótimo lugar para você contar a sua história e compartilhar sobre você.</p>
+                </Content>
+            </ContainerContent>
+            <ContainerContent>
+                <Content>
+                    <Emphasis>2035 - Atual</Emphasis>
+                    <p>CARGO</p>
+                    <div>
+                        <p>Nome da empresa</p>
+                        <p>Localização da empresa</p>
+                    </div>
+                    <p>Sou um parágrafo. Aqui você pode adicionar seu texto. É fácil, basta clicar em Editar texto ou clicar duas vezes sobre mim para editar o conteúdo, a fonte e mais.</p>
+                    <p>Sou um ótimo lugar para você contar a sua história e compartilhar sobre você.</p>
+                </Content>
+            </ContainerContent>
              
-//         </Container>
-//     )
-// }
+        </Container>
+    )
+}
