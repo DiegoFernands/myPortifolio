9b777527abbfcf6434853887cf5a1127f3b81979
diego <diego.fernandesjs@gmail.com>
2024-03-05 15:01:57 -0300

    add contact

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,8 +1,37 @@
+'use client'
+import Contact from "@/components/Main/Contato/contact";
+import styled from "styled-components";
+
 interface ContatoProps {
 
 }
+
+const Container =  styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #e1e6e3;
+`
+const Title = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 3rem 0 2rem 0;
+`
+const Square = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: #274862;
+`
+
 export default function Contato(props : ContatoProps){
     return(
-        <h1>Contato</h1>
+        <Container>
+            <Title>
+                <Square></Square>
+                <h1>Contato</h1>
+            </Title>
+            <Contact/>
+        </Container>
     )
 }
---
+++ src/components/Main/Contato/contact.tsx
@@ -0,0 +1,77 @@
+
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+    width: 90%;
+    margin-bottom: 2rem;
+`
+
+const Form = styled.div`
+    display: flex;
+    margin: 1rem;
+    gap: 1rem;
+    flex-direction: column;
+    width: 100%;
+`
+const DivName = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
+const DivLink = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+`
+
+const Link = styled.a`
+    height: 32px;
+    width: 130px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    font-weight: 600;
+    margin-top: 1.5rem;
+    background-color: #274862;
+    color: #fff;
+`
+
+
+export default function Contact(){
+    return(
+        <Container>
+            <Form>
+                <DivName>
+                    <div>
+                        <label>Nome</label>
+                        <input type="text" name="Nome"/>
+                    </div>
+                    <div>
+                        <label>Sobrenome</label>
+                        <input type="text" name="Sobrenome"/>
+                    </div>
+                </DivName>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="Email"/>
+                </div>
+                <div>
+                    <label>Assunto</label>
+                    <input type="text" name="Assunto"/>
+                </div>
+                <div>
+                    <label>Mensagem</label>
+                    <input type="text" name="Mensagem"/>
+                </div>
+                <DivLink>
+                    <Link href="#">Enviar</Link>
+                </DivLink>
+            </Form>
+        </Container>
+    )
+}
